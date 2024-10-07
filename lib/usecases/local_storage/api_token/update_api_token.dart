7efa132a211b6e09a4a6abadd2994fd0297a1f7a
Vinicius <timelord1025@gmail.com>
2024-10-07 15:58:47 -0300

    log

--- lib/usecases/local_storage/api_token/update_api_token.dart
+++ lib/usecases/local_storage/api_token/update_api_token.dart
@@ -1,8 +1,10 @@
 import '../../../data/dynamic_factories.dart';
 import '../../../factories/http_client.dart';
+import '../../../factories/logger.dart';
 
 Future<void> updateApiToken(String token) async {
   var rep = DynamicFactories.localStorage;
   await rep.saveApiToken(token);
+  logger.info('Setting api token: "$token"');
   httpClient.options.headers['Authorization'] = 'Bearer $token';
 }
