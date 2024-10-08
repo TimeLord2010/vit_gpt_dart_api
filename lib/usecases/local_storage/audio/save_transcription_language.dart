841fae3c2e62f1bf2df6895a56db6392948186ab
Vinicius <timelord1025@gmail.com>
2024-10-08 19:31:29 -0300

    fix: set audio configuration in save usecases

--- lib/usecases/local_storage/audio/save_transcription_language.dart
+++ lib/usecases/local_storage/audio/save_transcription_language.dart
@@ -1,6 +1,8 @@
+import 'package:vit_gpt_dart_api/data/configuration.dart';
 import 'package:vit_gpt_dart_api/data/dynamic_factories.dart';
 
 Future<void> saveTranscriptionLanguage(String lang) async {
   var rep = DynamicFactories.localStorage;
   await rep.saveTranscriptionLanguage(lang);
+  VitGptConfiguration.transcriptionLanguage = lang;
 }
--- lib/usecases/local_storage/audio/save_tts_quality.dart
+++ lib/usecases/local_storage/audio/save_tts_quality.dart
@@ -1,6 +1,8 @@
+import 'package:vit_gpt_dart_api/data/configuration.dart';
 import 'package:vit_gpt_dart_api/data/dynamic_factories.dart';
 
 Future<void> saveTtsQuality(bool highQuality) async {
   var rep = DynamicFactories.localStorage;
   await rep.saveTtsQuality(highQuality);
+  VitGptConfiguration.useHighQualityTts = highQuality;
 }
