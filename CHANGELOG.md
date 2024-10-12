7667e251d3198bef2bd6a4455e409668411821f4
Vinicius <timelord1025@gmail.com>
2024-10-12 14:18:46 -0300

    fix: load non existent thread

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.2.2
+
+- Fixed bug when loading non existent thread.
+
 ## 1.2.1
 
 - Allow custom tts handler.
--- lib/usecases/threads/load_thread.dart
+++ lib/usecases/threads/load_thread.dart
@@ -1,8 +1,16 @@
+import 'package:dio/dio.dart';
+
 import '../../data/models/conversation.dart';
 import '../../factories/create_threads_repository.dart';
 
 Future<Conversation?> loadThread(String id) async {
-  var rep = createThreadsRepository();
-  var thread = await rep.load(id);
-  return thread;
+  try {
+    var rep = createThreadsRepository();
+    var thread = await rep.load(id);
+    return thread;
+  } on DioException catch (e) {
+    var code = e.response?.statusCode;
+    if (code == 404) return null;
+    rethrow;
+  }
 }
