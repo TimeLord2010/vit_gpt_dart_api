861eea9584f1a0b875a0186d991d27488eb973b1
Vinicius <timelord1025@gmail.com>
2024-11-07 17:50:21 -0300

    fix: export mic send mode

--- lib/data/enums/index.dart
+++ lib/data/enums/index.dart
@@ -4,6 +4,7 @@ export 'gpt_model.dart';
 export 'image_model.dart';
 export 'image_size.dart';
 export 'image_style.dart';
+export 'mic_send_mode.dart';
 export 'player_state.dart';
 export 'sender_type.dart';
 export 'voice_mode_status.dart';
