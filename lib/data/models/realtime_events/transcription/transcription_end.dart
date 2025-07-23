e7bf353f54c1bb6595afede19549757892a749f1
Arlysthon Freitas <arlysthonfreitas@Arlysthons-MacBook-Air.local>
2025-07-23 16:11:44 -0300

    feat: add created at to dart api

--- lib/data/models/realtime_events/transcription/transcription_end.dart
+++ lib/data/models/realtime_events/transcription/transcription_end.dart
@@ -4,10 +4,12 @@ class TranscriptionEnd {
   final String id;
   final Role role;
   final String content;
+  final DateTime? createdAt;
 
   TranscriptionEnd({
     required this.id,
     required this.role,
     required this.content,
+    this.createdAt
   });
 }
