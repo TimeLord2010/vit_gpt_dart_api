c51cd641e48b5c580fc66ef11d484bc0f4df04ee
Vinícius Cotta <timelord1025@gmail.com>
2025-08-27 09:54:22 -0300

    feat: removed flutter package (shared_preferences) on dart project

--- lib/data/interfaces/local_storage/local_storage_model.dart
+++ lib/data/interfaces/local_storage/local_storage_model.dart
@@ -1,12 +1,10 @@
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:vit_gpt_dart_api/data/interfaces/local_storage/thread_local_storage_model.dart';
 
 import '../../enums/gpt_model.dart';
 import 'audio_local_storage_model.dart';
 
-abstract class LocalStorageModel with AudioLocalStorageModel, ThreadLocalStorageModel {
-  SharedPreferences get preferences;
-
+abstract class LocalStorageModel
+    with AudioLocalStorageModel, ThreadLocalStorageModel {
   // MARK: API Key
 
   Future<void> saveApiToken(String token);
