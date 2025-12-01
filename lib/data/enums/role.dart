32e1cf6b1db746cc71b500beae6ca10b953bc6bd
Vinicius G <timelord1025@gmail.com>
2025-12-01 18:04:34 -0300

    feat: reverted last change

--- lib/data/enums/role.dart
+++ lib/data/enums/role.dart
@@ -1,20 +1,17 @@
 enum Role {
   user,
   assistant,
-  undefined,
   system;
 
   bool get isUser => this == user;
   bool get isAssistant => this == assistant;
   bool get isSystem => this == system;
-  bool get isUndefinded => this == undefined;
 
   factory Role.fromValue(String value) {
     return switch (value) {
       'user' => user,
       'assistant' => assistant,
       'ai' => assistant,
-      'undefined' => undefined,
       _ => system,
     };
   }
